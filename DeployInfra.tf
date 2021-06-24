284928c60ce32bdccc4cb870f771a84bf87cfd47
Jérôme Faucher <62873431+JeromeF99@users.noreply.github.com>
2021-06-24 12:21:31 +0200

    Update DeployInfra.tf

--- DeployInfra.tf
+++ DeployInfra.tf
@@ -48,6 +48,8 @@ resource "aws_subnet" "subnet-public-1" {
 
 ### AZ2
 resource "aws_subnet" "subnet-public-2" {
+  count = var.env == "prod" ? 1 : 0
+  
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "true"
@@ -59,6 +61,8 @@ resource "aws_subnet" "subnet-public-2" {
 
 ### AZ3
 resource "aws_subnet" "subnet-public-3" {
+  count = var.env == "prod" ? 1 : 0
+  
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = "true"
@@ -82,6 +86,8 @@ resource "aws_subnet" "subnet-private-1" {
 
 ### AZ2
 resource "aws_subnet" "subnet-private-2" {
+  count = var.env == "prod" ? 1 : 0
+  
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.5.0/24"
   map_public_ip_on_launch = "false"
@@ -93,6 +99,8 @@ resource "aws_subnet" "subnet-private-2" {
 
 ### AZ3
 resource "aws_subnet" "subnet-private-3" {
+  count = var.env == "prod" ? 1 : 0
+  
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.6.0/24"
   map_public_ip_on_launch = "false"
@@ -185,11 +193,15 @@ resource "aws_route_table_association" "public-1" {
 }
 
 resource "aws_route_table_association" "public-2" {
+  count = var.env == "prod" ? 1 : 0
+  
   subnet_id      = aws_subnet.subnet-public-2.id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "public-3" {
+  count = var.env == "prod" ? 1 : 0
+  
   subnet_id      = aws_subnet.subnet-public-3.id
   route_table_id = aws_route_table.public.id
 }
